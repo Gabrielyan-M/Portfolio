b160ab7fcdec5f098ee73013ae3a8781be0a75f4
Gabrielyan-M <gabrielyan.m77@gmail.com>
2025-01-22 00:10:06 +0400

    cupyClick.js

--- script/cupyClick.js
+++ script/cupyClick.js
@@ -1,9 +1,15 @@
 import { cardLinkContact } from './loadingDOM.js'
 
+const notificationCopied = document.querySelector('.lnik-copied-block')
+
 cardLinkContact.forEach(card => {
     card.addEventListener('click', () => {
-        console.log(card.attributes);
         navigator.clipboard.writeText(card.textContent)
+
+        notificationCopied.classList.add('lnik-copied')
+        setTimeout(() => {
+            notificationCopied.classList.remove('lnik-copied')
+        }, 2000)
     })
     
 })
